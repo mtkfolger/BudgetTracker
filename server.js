f7300f6ced62cd854f5f978c743783a39636636a
Michael Folger <folger@zoller-usa.com>
2021-10-19 12:49:13 -0500

    updated server.js with mongoose connect info

--- server.js
+++ server.js
@@ -15,11 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget-app',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 // routes
 app.use(require("./routes/api.js"));
 
